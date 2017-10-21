fdf9adb9383b56850d911b00e22e80217bd9e01e
jczetta <jczetta@gmail.com>
2017-10-21 13:45:32 -0400

    Initial setup for main_app file

---
+++ main_app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, render_template, session, redirect, url_for
+from flask_script import Manager, Shell
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+
+# Configure base directory of app
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Application configurations
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Set up Flask debug stuff
+manager = Manager(app)
+moment = Moment(app) # For time
+db = SQLAlchemy(app) # For database use
+migrate = Migrate(app, db) # For database use
+
+#########
+######### Everything above this line is important setup, not problem-solving.
+#########
+
+##### Set up Models #####
+
+
+
+##### Set up Forms #####
+
+
+##### Set up Controllers #####
+
+
