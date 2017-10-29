5ec407691508021cd3a5f876b32debcbb1f11450
jczetta <jczetta@gmail.com>
2017-10-29 14:49:31 -0400

    Update to two table 1:many relationship, postgres, links, gitignore

--- main_app.py
+++ main_app.py
@@ -3,7 +3,7 @@
 # 1:Many relationship: Album:Song(s)
 # Many:Many relationship: Artists:Songs
 # TODO: E-R diagram to display
-# TODO: relationship to build
+# TODO: relationship to build (done, ish)
 # TODO: relationship and association table to build
 
 import os
@@ -26,8 +26,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'hardtoguessstringfromsi364thisisnotsupersecurebutitsok'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite') # Determining where your database file will be stored, and what it will be called
+# app.config['SQLALCHEMY_DATABASE_URI'] =\
+    # 'sqlite:///' + os.path.join(basedir, 'data.sqlite') # Determining where your database file will be stored, and what it will be called
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/songs_data"
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -57,7 +58,12 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 
 ##### Set up Models #####
 
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+
 class Artist(db.Model):
+    __tablename__ = "artists"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     songs = db.relationship('Song',backref='Artist')
@@ -65,11 +71,10 @@ class Artist(db.Model):
     def __repr__(self):
         return "{} (ID: {})".format(self.name,self.id)
 
-
 class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64),unique=True) # Only unique title songs
-    artist_id = db.Column(db.Integer, db.ForeignKey(Artist.id)) # changed
+    artist_id = db.Column(db.Integer, db.ForeignKey("artists.id")) # changed
     genre = db.Column(db.String(64))
 
     def __repr__(self):
@@ -88,16 +93,30 @@ class SongForm(FlaskForm):
 
 ##### Helper functions
 
-### For database additions / get_or_create
+### For database additions / get_or_create functions
+
+def get_or_create_artist(db_session,artist_name):
+    artist = db_session.query(Artist).filter_by(name=artist_name).first()
+    if artist:
+        return artist
+    else:
+        artist = Artist(name=artist_name)
+        db_session.add(artist)
+        db_session.commit()
+        return artist
+
 def get_or_create_song(db_session, song_title, song_artist, song_genre):
     song = db_session.query(Song).filter_by(title=song_title).first()
     if song:
         return song
     else:
-        song = Song(title=song_title,artist=song_artist,genre=song_genre)
+        artist = get_or_create_artist(db_session, song_artist)
+        song = Song(title=song_title,genre=song_genre,artist_id=artist.id)
         db_session.add(song)
         db_session.commit()
         return song
+
+
 
 
 ##### Set up Controllers (view functions) #####
@@ -131,9 +150,15 @@ def see_all():
     all_songs = [] # To be tuple list of title, genre
     songs = Song.query.all()
     for s in songs:
-        all_songs.append((s.title,s.artist, s.genre))
+        artist = Artist.query.filter_by(id=s.artist_id).first()
+        all_songs.append((s.title,artist.name, s.genre))
     return render_template('all_songs.html',all_songs=all_songs)
 
+@app.route('/all_artists')
+def see_all_artists():
+    artists = Artist.query.all()
+    names = [(a.name, len(Song.query.filter_by(artist_id=a.id).all())) for a in artists]
+    return render_template('all_artists.html',artist_names=names)
 
 if __name__ == '__main__':
     db.create_all()
